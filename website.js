ece1b498b54703f9009cf57a35d35512cb5537c9
AlexMcHale24 <am2015@hw.ac.uk>
2024-01-30 22:58:01 +0000

    Recent

--- website.js
+++ website.js
@@ -28,3 +28,17 @@ document.addEventListener("scroll", function () {
     // start scrolling
     scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
 }
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
