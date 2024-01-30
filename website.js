0646e1aeed4027ba5d9ed7577c7043b6f969030e
AlexMcHale24 <am2015@hw.ac.uk>
2024-01-30 21:03:24 +0000

    Scroll to section.1

--- website.js
+++ website.js
@@ -1,9 +1,29 @@
 document.addEventListener("scroll", function () {
     const navbar = document.querySelector(".navi");
-    const navbarHeight = 100;
-    const distanceFromTop = Math.abs(
-      document.body.getBoundingClientRect().top
-    );
+    const navbarHeight = 5;
+    const distanceFromTop = Math.abs(document.body.getBoundingClientRect().top);
     if (distanceFromTop >= navbarHeight) navbar.classList.add("scroll-top");
     else navbar.classList.remove("scroll-top");
   });
+  window.smoothScroll = function(target) {
+    var scrollContainer = target;
+    do { //find scroll container
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+
+    var targetY = 0;
+    do { //find the top of target relatively to the container
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    scroll = function(c, a, b, i) {
+        i++; if (i > 30) return;
+        c.scrollTop = a + (b - a) / 30 * i;
+        setTimeout(function(){ scroll(c, a, b, i); }, 20);
+    }
+    // start scrolling
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}
